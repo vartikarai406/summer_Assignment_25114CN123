51413190c09a7b36d5873b229f6d4017894e8145
VARTIKA RAI <vartikarai406@gmail.com>
2026-06-02 10:53:07 +0530

    Added DAY-6 files

---
+++ DAY-6/setsbits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        if (n % 2 == 1)
+            count++;
+
+        n /= 2;
+    }
+
+    printf("Number of set bits = %d", count);
+
+    return 0;
+}
