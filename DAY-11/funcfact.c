78123abe3b09f6be892e8bb42c2b6ea07e9a4a86
VARTIKA RAI <vartikarai406@gmail.com>
2026-06-07 10:05:32 +0530

    Added DAY-11 files

---
+++ DAY-11/funcfact.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    long long fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        printf("Factorial is not defined for negative numbers.");
+    else
+        printf("Factorial of %d = %lld", num, factorial(num));
+
+    return 0;
+}
---
+++ DAY-11/funprimeno.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n <= 1)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num))
+        printf("%d is a Prime Number", num);
+    else
+        printf("%d is not a Prime Number", num);
+
+    return 0;
+}
